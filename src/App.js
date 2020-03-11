fcae5a4f6d2ffd4b8f68e15f6ded212c3795532c
Lucas Siqueira <lucassiqzro@gmail.com>
2020-03-11 07:48:27 -0300

    Create input component

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import { useSelector } from 'react-redux';
 import createRouter from './routes';
 
 export default function App() {
-  // const signed = useSelector(state => state.auth.signed);
-  const signed = true;
+  const signed = useSelector(state => state.auth.signed);
 
   return createRouter(signed);
 }
---
+++ src/components/Input/styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  padding: 0 15px;
+  height: 46px;
+  background: #fff;
+  border-radius: 4px;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const TInput = styled.TextInput.attrs({
+  placeholderTextColor: '#999999',
+})`
+  flex: 1;
+  font-size: 15px;
+  margin-left: 10px;
+  color: #fff;
+`;
