fc9bf7525c1b51f592ab6bebc66a7863c4e6f1cf
Lucas Siqueira <lucassiqzro@gmail.com>
2020-03-15 01:38:39 -0300

    Create show problems page

--- src/pages/ShowProblems/index.js
+++ src/pages/ShowProblems/index.js
@@ -1,14 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'react-native';
+import { useRoute } from '@react-navigation/native';
+import { parseISO, format } from 'date-fns';
 
-import { Container, Background, Content } from './styles';
+import api from '../../services/api';
+
+import {
+  Container,
+  Background,
+  Content,
+  Card,
+  Description,
+  CreatedDate,
+  Title,
+  ProblemList,
+} from './styles';
 
 export default function ShowProblems() {
+  const route = useRoute();
+  const [problems, setProblems] = useState([{}]);
+  const { order } = route.params;
+
+  useEffect(() => {
+    async function loadInitialData() {
+      const response = await api.get(`delivery/${order.id}/problems`);
+
+      const data = response.data.map(problem => ({
+        ...problem,
+        create_date_formated: format(parseISO(problem.createdAt), 'dd/MM/yyyy'),
+      }));
+
+      setProblems(data);
+    }
+
+    loadInitialData();
+  }, []);
+
   return (
     <Container>
       <StatusBar barStyle="light-content" backgroundColor="#7D40E7" />
       <Background />
-      <Content />
+      <Content>
+        <Title>Encomenda {order.id}</Title>
+        <ProblemList>
+          {problems.map(problem => (
+            <Card>
+              <Description>{problem.description}</Description>
+              <CreatedDate>{problem.create_date_formated}</CreatedDate>
+            </Card>
+          ))}
+        </ProblemList>
+      </Content>
     </Container>
   );
 }
--- src/pages/ShowProblems/styles.js
+++ src/pages/ShowProblems/styles.js
@@ -11,5 +11,42 @@ export const Background = styled.View`
 `;
 
 export const Content = styled.View`
-  margin-top: -63px;
+  margin-top: -83px;
+  padding-left: 20px;
+  padding-right: 20px;
+`;
+export const Title = styled.Text`
+  font-size: 18px;
+  align-self: center;
+  color: #fff;
+  font-weight: bold;
+`;
+
+export const ProblemList = styled.ScrollView.attrs({
+  showsVerticalScrollIndicator: false,
+})`
+  margin-top: 13px;
+`;
+
+export const Card = styled.View`
+  border: 1px #0000001a;
+  margin-bottom: 15px;
+  padding: 19px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  background: #fff;
+  border-radius: 4px;
+`;
+
+export const Description = styled.Text`
+  font-size: 16px;
+  max-width: 260px;
+  color: #999999;
+`;
+
+export const CreatedDate = styled.Text`
+  font-size: 12px;
+  color: #c1c1c1;
 `;
