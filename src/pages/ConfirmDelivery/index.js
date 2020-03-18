ebe56342d8b27b41189f0113bfec6859452f5cbf
Lucas Siqueira <lucassiqzro@gmail.com>
2020-03-18 00:16:18 -0300

    Create confirm delivery page

--- src/pages/ConfirmDelivery/index.js
+++ src/pages/ConfirmDelivery/index.js
@@ -1,14 +1,97 @@
-import React from 'react';
-import { StatusBar } from 'react-native';
+import React, { useState, useRef } from 'react';
+import { Alert, StatusBar } from 'react-native';
+import { useRoute, useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container, Background, Content } from './styles';
+import api from '../../services/api';
+
+import {
+  Container,
+  Background,
+  Content,
+  CameraContainer,
+  SubmitButton,
+  Camera,
+  Preview,
+  PhotoContainer,
+  TakePhotoButton,
+} from './styles';
 
 export default function ConfirmDelivery() {
+  const navigation = useNavigation();
+  const route = useRoute();
+  const { order } = route.params;
+  const [picture, setPicture] = useState('');
+  const cameraRef = useRef(null);
+
+  async function handleSubmit() {
+    const dataFile = new FormData();
+    dataFile.append('file', {
+      type: 'image/jpg',
+      uri: picture,
+      name: 'signature.jpg',
+    });
+
+    try {
+      const response = await api.post('files', dataFile);
+
+      await api.put(
+        `deliveryman/${order.deliveryman_id}/deliveries/${order.id}`,
+        {
+          end_date: new Date(),
+          signature_id: response.data.id,
+        },
+      );
+
+      navigation.popToTop();
+
+      Alert.alert('Entrega finalizada com sucesso!');
+    } catch (err) {
+      Alert.alert(
+        'Falha ao confirmar a entrga',
+        'Houve um erro ao confirmar entrega, tente mais tarde!',
+      );
+    }
+  }
+
+  async function handletakePicture() {
+    const options = { quality: 0.5, base64: true };
+    const data = await cameraRef.current.takePictureAsync(options);
+    await setPicture(data.uri);
+  }
+
   return (
     <Container>
       <StatusBar barStyle="light-content" backgroundColor="#7D40E7" />
       <Background />
-      <Content />
+      <Content>
+        {picture === '' ? (
+          <CameraContainer>
+            <Camera ref={cameraRef} type="back" captureAudio={false} />
+            <TakePhotoButton onPress={handletakePicture}>
+              <Icon
+                name="photo-camera"
+                size={35}
+                color="rgba(255,255,255,0.6)"
+              />
+            </TakePhotoButton>
+          </CameraContainer>
+        ) : (
+          <PhotoContainer>
+            <Preview source={{ uri: picture }} />
+            <TakePhotoButton onPress={() => setPicture('')}>
+              <Icon
+                name="photo-camera"
+                size={35}
+                color="rgba(255,255,255,0.6)"
+              />
+            </TakePhotoButton>
+          </PhotoContainer>
+        )}
+        <SubmitButton onPress={handleSubmit} loading={false}>
+          Enviar
+        </SubmitButton>
+      </Content>
     </Container>
   );
 }
--- src/pages/ConfirmDelivery/styles.js
+++ src/pages/ConfirmDelivery/styles.js
@@ -1,4 +1,7 @@
 import styled from 'styled-components/native';
+import { RNCamera } from 'react-native-camera';
+
+import Button from '../../components/Button';
 
 export const Container = styled.View`
   background: #fff;
@@ -12,4 +15,47 @@ export const Background = styled.View`
 
 export const Content = styled.View`
   margin-top: -63px;
+  padding-left: 20px;
+  padding-right: 20px;
+  flex: 1;
+`;
+
+export const CameraContainer = styled.View`
+  width: 100%;
+  height: 88%;
+  border-radius: 4px;
+  overflow: hidden;
+`;
+
+export const PhotoContainer = styled.View`
+  width: 100%;
+  height: 88%;
+  border-radius: 4px;
+`;
+
+export const Preview = styled.Image`
+  width: 100%;
+  height: 100%;
+  border-radius: 4px;
+`;
+
+export const Camera = styled(RNCamera)`
+  flex: 1;
+`;
+
+export const SubmitButton = styled(Button)`
+  background: #7159c1;
+  margin-top: 15px;
+`;
+
+export const TakePhotoButton = styled.TouchableOpacity`
+  margin-top: -100px;
+  background: #0000004d;
+  width: 60px;
+  height: 60px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 30px;
+  align-self: center;
+  margin-bottom: 22px;
 `;
