f5939f9e278b676f768262d2da3943c364167dcd
Lucas Siqueira <lucassiqzro@gmail.com>
2020-03-18 00:32:10 -0300

    Create function to confirm withdraw

--- src/pages/OrderDetail/index.js
+++ src/pages/OrderDetail/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
+import { StatusBar, Alert } from 'react-native';
 import { useRoute, useNavigation } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { parseISO, format } from 'date-fns';
+
+import api from '../../services/api';
 
 import {
   Container,
@@ -25,6 +27,26 @@ export default function OrderDetail() {
   const navigation = useNavigation();
   const route = useRoute();
   const { order } = route.params;
+
+  async function handleWithdraw() {
+    try {
+      await api.put(
+        `deliveryman/${order.deliveryman_id}/deliveries/${order.id}`,
+        {
+          start_date: new Date(),
+        },
+
+        Alert.alert(
+          'Retirada feita com sucesso!, Os status da encomanda foram atualizados!',
+        ),
+      );
+    } catch (err) {
+      Alert.alert(
+        'Falha ao confirmar a entrga',
+        'Verifique o horario para retirada, tente mais tarde!',
+      );
+    }
+  }
 
   return (
     <Container>
@@ -72,30 +94,42 @@ export default function OrderDetail() {
             </DateItem>
           </DatesContainer>
         </Card>
-        <OptionsContainer>
-          <Option
-            onPress={() => navigation.navigate('InformProblem', { order })}>
-            <Icon name="close-circle-outline" size={25} color="#E74040" />
-            <OptionTitle> Informar{'\n'}Problema</OptionTitle>
-          </Option>
-          <OptionMiddle
-            onPress={() => navigation.navigate('ShowProblems', { order })}>
-            <Icon name="information-outline" size={25} color="#E7BA40" />
-            <OptionTitle> Visualizar{'\n'}Problemas</OptionTitle>
-          </OptionMiddle>
-          {order.start_date ? (
+        {!order.end_date ? (
+          <OptionsContainer>
             <Option
-              onPress={() => navigation.navigate('ConfirmDelivery', { order })}>
-              <Icon name="check-circle-outline" size={25} color="#7D40E7" />
-              <OptionTitle>Confirmar{'\n\t'}Entrega</OptionTitle>
+              onPress={() => navigation.navigate('InformProblem', { order })}>
+              <Icon name="close-circle-outline" size={25} color="#E74040" />
+              <OptionTitle> Informar{'\n'}Problema</OptionTitle>
             </Option>
-          ) : (
-            <Option>
-              <Icon name="check-circle-outline" size={25} color="#7D40E7" />
-              <OptionTitle>Confirmar{'\n\t'}Retirada</OptionTitle>
-            </Option>
-          )}
-        </OptionsContainer>
+            <OptionMiddle
+              onPress={() => navigation.navigate('ShowProblems', { order })}>
+              <Icon name="information-outline" size={25} color="#E7BA40" />
+              <OptionTitle> Visualizar{'\n'}Problemas</OptionTitle>
+            </OptionMiddle>
+            {order.start_date ? (
+              <Option
+                onPress={() =>
+                  navigation.navigate('ConfirmDelivery', { order })
+                }>
+                <Icon name="check-circle-outline" size={25} color="#7D40E7" />
+                <OptionTitle>Confirmar{'\n\t'}Entrega</OptionTitle>
+              </Option>
+            ) : (
+              <Option onPress={handleWithdraw}>
+                <Icon name="check-circle-outline" size={25} color="#7D40E7" />
+                <OptionTitle>Confirmar{'\n\t'}Retirada</OptionTitle>
+              </Option>
+            )}
+          </OptionsContainer>
+        ) : (
+          <OptionsContainer>
+            <OptionMiddle
+              onPress={() => navigation.navigate('ShowProblems', { order })}>
+              <Icon name="information-outline" size={25} color="#E7BA40" />
+              <OptionTitle> Visualizar{'\n'}Problemas</OptionTitle>
+            </OptionMiddle>
+          </OptionsContainer>
+        )}
       </Content>
     </Container>
   );
